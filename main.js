38e778776c8072a57a99e79e0de72b3bef66ae3d
Franco Gonzalez <gonzalez.francodavid@hotmail.com>
2019-10-10 21:14:53 -0300

    cambios hechos por franco

--- main.js
+++ main.js
@@ -12,3 +12,10 @@
       var num = Math.floor(Math.random() * (max - min)) + min;
       return num;
   }
+
+  Ball.prototype.draw = function() {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();
+  }
